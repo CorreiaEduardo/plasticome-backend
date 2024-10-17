7290bd2375d6a02603721627e01847398482d903
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-10-17 20:51:32 -0300

    chore(start.sh): adiciona parametro para desabilitar tls do dockerd

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 celery -A plasticome.config.celery worker -l info --pool=solo &
-dockerd --host=tcp://0.0.0.0:2375 --host=unix:///var/run/docker.sock &
+dockerd --tls=false --host=tcp://0.0.0.0:2375 --host=unix:///var/run/docker.sock &
 cd /app && flask run -h backend
 # celery -A plasticome.config.celery worker -l info
